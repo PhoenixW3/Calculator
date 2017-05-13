e96b528474495d16ed0566120cd7310e9f91c8d8
PhoenixW3 <PhoenixWarr1997@gmail.com>
2017-05-13 11:00:29 -0700

    Button click to result
    This is where the button click will show to the result

---
+++ Win_Calculator/Win_Calculator/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            Result.Text = Result.Text + b.Text;
+        }
+    }
+}
